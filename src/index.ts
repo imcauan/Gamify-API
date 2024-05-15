33e51ba23ae7b85d37d982a471e45f397727875c
Cauan Diniz <cauanrodiniz@gmail.com>
2024-05-15 15:35:59 -0300

    feat: add community and post crud

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
-import express from "express";
+import express, { Router } from "express";
 import cors from "cors";
 
 const app = express();
 const AuthController = require("../src/app/auth/AuthController");
 const UserController = require("../src/app/user/UserController");
+const PostController = require("../src/app/posts/PostController")
+const CommunityController = require("../src/app/community/CommunityController");
 const authMiddleware = require("../src/middlewares/authMiddleware");
-
 app.use(express.json());
 app.use(cors({
     origin: "http://localhost:3000"
@@ -17,5 +18,13 @@ app.listen(3333, () => {
 app.post("/user/signin", AuthController.signIn);
 app.post("/user/signup", AuthController.signUp);
 
-app.use(authMiddleware);
-app.get("/profile", authMiddleware, UserController.getProfile);
+app.use(() => authMiddleware)
+app.get("/user/me", UserController.getProfile);
+
+app.post("/new/post", PostController.create);
+app.put("/post/:id", PostController.update);
+app.delete("/delete/post/:id", PostController.delete);
+
+app.post("/new/community", CommunityController.create);
+app.put("/community/:id", CommunityController.update);
+app.delete("/delete/community/:id", CommunityController.delete);
